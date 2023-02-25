50165788b5ea2faf3323064ad29ea8716530559d
Ayah Elsais <elsais.ayah@gmail.com>
2023-02-25 16:27:26 -0500

    done

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -35,14 +35,34 @@ def print_spicy_foods(spicy_foods):
         print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
 
 
+# def get_spicy_food_by_cuisine(spicy_foods, cuisine):
+#     foods = [food for food in spicy_foods if food["cuisine"] == cuisine]
+#     return foods
+# this mthod returns multiple dictionaries
+
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+    return None
+
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = get_spiciest_foods(spicy_foods)
+    for food in spiciest_foods:
+        heat_level = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
+
 
 def get_average_heat_level(spicy_foods):
-    pass
+    total_heat_level = sum(food["heat_level"] for food in spicy_foods)
+    num_foods = len(spicy_foods)
+    if num_foods > 0:
+        return total_heat_level // num_foods
+    else:
+        return 0
 
-def create_spicy_food(spicy_foods, spicy_food):
-    pass
+def create_spicy_food(spicy_foods, new_food):
+    spicy_foods.append(new_food)
+    return spicy_foods
+
